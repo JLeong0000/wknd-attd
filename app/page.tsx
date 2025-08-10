6b19886e8d20f45d4dbb16ee9f35c6a44f57e342
Justin Leong <129262225+JLeong0000@users.noreply.github.com>
2025-08-10 17:30:25 +0800

    Added database listeners

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@
 
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { generateId, copyGenerate } from "./ts/helper";
-import { getPeople, postCurrPpl, postDefPpl } from "./ts/server";
+import {
+    getPeople,
+    postCurrPpl,
+    postDefPpl,
+    currentPplChangeListener,
+    defaultPplChangeListener,
+} from "./ts/server";
 import { IoPersonAddSharp } from "react-icons/io5";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
@@ -10,197 +16,249 @@ import Person from "./components/Person";
 import { PersonData } from "./types";
 
 const Home: React.FC = () => {
-	const [currentPpl, setCurrentPpl] = useState<PersonData[]>([]);
-	const [defaultPpl, setDefaultPpl] = useState<PersonData[]>([]);
-	const [tempDefPpl, setTempDefPpl] = useState<PersonData[]>([]);
-	const [isSaving, setIsSaving] = useState(false);
-	const [isEditing, setIsEditing] = useState(false);
-	const [currModified, setCurrModified] = useState(false);
-	const [tempModified, setTempModified] = useState(false);
+    const [currentPpl, setCurrentPpl] = useState<PersonData[]>([]);
+    const [defaultPpl, setDefaultPpl] = useState<PersonData[]>([]);
+    const [tempDefPpl, setTempDefPpl] = useState<PersonData[]>([]);
+    const [isSaving, setIsSaving] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [currModified, setCurrModified] = useState(false);
+    const [tempModified, setTempModified] = useState(false);
 
-	const initPpl = useCallback(async () => {
-		const { currentPeople, defaultPeople } = await getPeople();
-		setCurrentPpl(currentPeople);
-		setDefaultPpl(defaultPeople);
-	}, []);
+    const initPpl = useCallback(async () => {
+        const { currentPeople, defaultPeople } = await getPeople();
+        setCurrentPpl(currentPeople);
+        setDefaultPpl(defaultPeople);
+    }, []);
 
-	const toggleEditing = () => {
-		if (isEditing) {
-			setIsEditing(false);
-		} else {
-			setTempDefPpl(defaultPpl);
-			setIsEditing(true);
-		}
-	};
+    const toggleEditing = () => {
+        if (isEditing) {
+            setIsEditing(false);
+        } else {
+            setTempDefPpl(defaultPpl);
+            setIsEditing(true);
+        }
+    };
 
-	const resetCurrent = () => {
-		setCurrModified(true);
-		setCurrentPpl(defaultPpl);
-	};
+    const resetCurrent = () => {
+        setCurrModified(true);
+        setCurrentPpl(defaultPpl);
+    };
 
-	const updateDefault = async (): Promise<void> => {
-		setIsSaving(true);
-		setDefaultPpl(tempDefPpl);
+    const updateDefault = async (): Promise<void> => {
+        setIsSaving(true);
+        setDefaultPpl(tempDefPpl);
 
-		const saveStatus = await postDefPpl(tempDefPpl);
-		setIsSaving(false);
-		setIsEditing(!saveStatus);
-	};
+        const saveStatus = await postDefPpl(tempDefPpl);
+        setIsSaving(false);
+        setIsEditing(!saveStatus);
+    };
 
-	const saveCurrent = async (): Promise<void> => {
-		setIsSaving(true);
-		setCurrModified(false);
-		await postCurrPpl(currentPpl);
-		setIsSaving(false);
-	};
+    const saveCurrent = async (): Promise<void> => {
+        setIsSaving(true);
+        setCurrModified(false);
+        await postCurrPpl(currentPpl);
+        setIsSaving(false);
+    };
 
-	const handleStatusChange = useCallback((id: string, newStatus: string, isEditing: boolean) => {
-		if (isEditing) {
-			setTempModified(true);
-			setTempDefPpl(prev => prev?.map(person => (person.id === id ? { ...person, status: newStatus } : person)));
-		} else {
-			setCurrModified(true);
-			setCurrentPpl(prev => prev?.map(person => (person.id === id ? { ...person, status: newStatus } : person)));
-		}
-	}, []);
+    const handleStatusChange = useCallback(
+        (id: string, newStatus: string, isEditing: boolean) => {
+            if (isEditing) {
+                setTempModified(true);
+                setTempDefPpl((prev) =>
+                    prev?.map((person) =>
+                        person.id === id
+                            ? { ...person, status: newStatus }
+                            : person
+                    )
+                );
+            } else {
+                setCurrModified(true);
+                setCurrentPpl((prev) =>
+                    prev?.map((person) =>
+                        person.id === id
+                            ? { ...person, status: newStatus }
+                            : person
+                    )
+                );
+            }
+        },
+        []
+    );
 
-	const handleNameChange = useCallback((id: string, newName: string, isEditing: boolean) => {
-		if (isEditing) {
-			setTempModified(true);
-			setTempDefPpl(prev => prev?.map(person => (person.id === id ? { ...person, name: newName } : person)));
-		} else {
-			setCurrModified(true);
-			setCurrentPpl(prev => prev?.map(person => (person.id === id ? { ...person, name: newName } : person)));
-		}
-	}, []);
+    const handleNameChange = useCallback(
+        (id: string, newName: string, isEditing: boolean) => {
+            if (isEditing) {
+                setTempModified(true);
+                setTempDefPpl((prev) =>
+                    prev?.map((person) =>
+                        person.id === id ? { ...person, name: newName } : person
+                    )
+                );
+            } else {
+                setCurrModified(true);
+                setCurrentPpl((prev) =>
+                    prev?.map((person) =>
+                        person.id === id ? { ...person, name: newName } : person
+                    )
+                );
+            }
+        },
+        []
+    );
 
-	const addPerson = () => {
-		const newPerson = {
-			id: generateId(),
-			name: "",
-			status: "S2: Sitting",
-		};
+    const addPerson = () => {
+        const newPerson = {
+            id: generateId(),
+            name: "",
+            status: "S2: Sitting",
+        };
 
-		if (isEditing) {
-			setTempModified(true);
-			setTempDefPpl(prev => [...(prev || []), newPerson]);
-		} else {
-			setCurrModified(true);
-			setCurrentPpl(prev => [...(prev || []), newPerson]);
-		}
-	};
+        if (isEditing) {
+            setTempModified(true);
+            setTempDefPpl((prev) => [...(prev || []), newPerson]);
+        } else {
+            setCurrModified(true);
+            setCurrentPpl((prev) => [...(prev || []), newPerson]);
+        }
+    };
 
-	const deletePerson = (id: string) => {
-		if (isEditing) {
-			setTempModified(true);
-			setTempDefPpl(prev => prev?.filter(person => person.id !== id));
-		} else {
-			setCurrModified(true);
-			setCurrentPpl(prev => prev?.filter(person => person.id !== id));
-		}
-	};
+    const deletePerson = (id: string) => {
+        if (isEditing) {
+            setTempModified(true);
+            setTempDefPpl((prev) => prev?.filter((person) => person.id !== id));
+        } else {
+            setCurrModified(true);
+            setCurrentPpl((prev) => prev?.filter((person) => person.id !== id));
+        }
+    };
 
-	const editingPeople = useMemo(() => {
-		if (tempDefPpl.length == 0) return;
-		return tempDefPpl?.map(person => (
-			<Person
-				key={person.id}
-				person={person}
-				handleStatusChange={handleStatusChange}
-				handleNameChange={handleNameChange}
-				deletePerson={deletePerson}
-				isEditing={true}
-			/>
-		));
-	}, [tempDefPpl, handleStatusChange, handleNameChange]);
+    const editingPeople = useMemo(() => {
+        if (tempDefPpl.length == 0) return;
+        return tempDefPpl?.map((person) => (
+            <Person
+                key={person.id}
+                person={person}
+                handleStatusChange={handleStatusChange}
+                handleNameChange={handleNameChange}
+                deletePerson={deletePerson}
+                isEditing={true}
+            />
+        ));
+    }, [tempDefPpl, handleStatusChange, handleNameChange]);
 
-	const currentPeople = useMemo(() => {
-		if (currentPpl.length == 0) return;
-		return currentPpl?.map(person => (
-			<Person
-				key={person.id}
-				person={person}
-				handleStatusChange={handleStatusChange}
-				handleNameChange={handleNameChange}
-				deletePerson={deletePerson}
-				isEditing={false}
-			/>
-		));
-	}, [currentPpl, handleStatusChange, handleNameChange]);
+    const currentPeople = useMemo(() => {
+        if (currentPpl.length == 0) return;
+        return currentPpl?.map((person) => (
+            <Person
+                key={person.id}
+                person={person}
+                handleStatusChange={handleStatusChange}
+                handleNameChange={handleNameChange}
+                deletePerson={deletePerson}
+                isEditing={false}
+            />
+        ));
+    }, [currentPpl, handleStatusChange, handleNameChange]);
 
-	useEffect(() => {
-		initPpl();
-	}, [initPpl]);
+    useEffect(() => {
+        initPpl();
+        currentPplChangeListener();
+        defaultPplChangeListener();
+    }, [initPpl]);
 
-	return (
-		<main>
-			<div className="min-h-screen my-auto flex items-center justify-center">
-				<div className="w-full max-w-lg bg-white rounded-xl shadow-lg p-4 space-y-7 sm:my-10">
-					<header>
-						<p className="text-4xl font-bold text-slate-800 text-center">Attendance Generator</p>
-						<p className="text-center text-slate-500 mt-1">Update status for each person</p>
-					</header>
+    return (
+        <main>
+            <div className="min-h-screen my-auto flex items-center justify-center">
+                <div className="w-full max-w-lg bg-white rounded-xl shadow-lg p-4 space-y-7 sm:my-10">
+                    <header>
+                        <p className="text-4xl font-bold text-slate-800 text-center">
+                            Attendance Generator
+                        </p>
+                        <p className="text-center text-slate-500 mt-1">
+                            Update status for each person
+                        </p>
+                    </header>
 
-					<div className="flex gap-2">
-						<button
-							onClick={resetCurrent}
-							disabled={isEditing}
-							className={`w-full text-white font-bold py-2 px-4 rounded-lg ${isEditing ? "bg-zinc-700" : "bg-red-800 hover:bg-red-700 cursor-pointer"} `}
-						>
-							Reset
-						</button>
-						<button
-							onClick={() => toggleEditing()}
-							className={`w-full bg-indigo-900 hover:bg-indigo-800 font-bold py-2 px-4 rounded-lg cursor-pointer 
+                    <div className="flex gap-2">
+                        <button
+                            onClick={resetCurrent}
+                            disabled={isEditing}
+                            className={`w-full text-white font-bold py-2 px-4 rounded-lg ${
+                                isEditing
+                                    ? "bg-zinc-700"
+                                    : "bg-red-800 hover:bg-red-700 cursor-pointer"
+                            } `}
+                        >
+                            Reset
+                        </button>
+                        <button
+                            onClick={() => toggleEditing()}
+                            className={`w-full bg-indigo-900 hover:bg-indigo-800 font-bold py-2 px-4 rounded-lg cursor-pointer 
 								${isEditing ? "text-blue-400" : "text-white"} `}
-						>
-							{isEditing ? "Editing" : "Edit Default"}
-						</button>
-					</div>
+                        >
+                            {isEditing ? "Editing" : "Edit Default"}
+                        </button>
+                    </div>
 
-					<div className="space-y-3">
-						{isEditing ? editingPeople : currentPeople}
+                    <div className="space-y-3">
+                        {isEditing ? editingPeople : currentPeople}
 
-						<button
-							onClick={addPerson}
-							className="flex items-center justify-center w-full p-3 text-xl text-zinc-500 rounded-lg cursor-pointer border border-slate-200 bg-slate-100 hover:bg-slate-200"
-						>
-							<IoPersonAddSharp />
-						</button>
-					</div>
+                        <button
+                            onClick={addPerson}
+                            className="flex items-center justify-center w-full p-3 text-xl text-zinc-500 rounded-lg cursor-pointer border border-slate-200 bg-slate-100 hover:bg-slate-200"
+                        >
+                            <IoPersonAddSharp />
+                        </button>
+                    </div>
 
-					{isEditing ? (
-						<button
-							onClick={updateDefault}
-							disabled={!tempModified}
-							className={`flex justify-center w-full text-white font-bold py-3 px-4 rounded-lg transition-all duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 
-                                ${tempModified ? "bg-violet-600 hover:bg-violet-700 active:scale-95 cursor-pointer" : "bg-zinc-700"}
+                    {isEditing ? (
+                        <button
+                            onClick={updateDefault}
+                            disabled={!tempModified}
+                            className={`flex justify-center w-full text-white font-bold py-3 px-4 rounded-lg transition-all duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 
+                                ${
+                                    tempModified
+                                        ? "bg-violet-600 hover:bg-violet-700 active:scale-95 cursor-pointer"
+                                        : "bg-zinc-700"
+                                }
                                 `}
-						>
-							{isSaving ? <AiOutlineLoading3Quarters className="animate-spin text-2xl" /> : "Update Default"}
-						</button>
-					) : (
-						<div className="flex flex-col gap-2">
-							<button
-								onClick={saveCurrent}
-								disabled={!currModified}
-								className={`flex justify-center w-full text-white font-bold py-3 px-4 rounded-lg transition-all duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 
-                                    ${currModified ? "bg-violet-600 hover:bg-violet-700 active:scale-95 cursor-pointer" : "bg-zinc-700"}`}
-							>
-								{isSaving ? <AiOutlineLoading3Quarters className="animate-spin text-2xl" /> : "Save"}
-							</button>
-							<button
-								onClick={() => copyGenerate(currentPpl)}
-								className="w-full text-white font-bold py-3 px-4 rounded-lg cursor-pointer bg-blue-600 hover:bg-blue-700 active:scale-95 transition-all duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-							>
-								Generate Message
-							</button>
-						</div>
-					)}
-				</div>
-			</div>
-		</main>
-	);
+                        >
+                            {isSaving ? (
+                                <AiOutlineLoading3Quarters className="animate-spin text-2xl" />
+                            ) : (
+                                "Update Default"
+                            )}
+                        </button>
+                    ) : (
+                        <div className="flex flex-col gap-2">
+                            <button
+                                onClick={saveCurrent}
+                                disabled={!currModified}
+                                className={`flex justify-center w-full text-white font-bold py-3 px-4 rounded-lg transition-all duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 
+                                    ${
+                                        currModified
+                                            ? "bg-violet-600 hover:bg-violet-700 active:scale-95 cursor-pointer"
+                                            : "bg-zinc-700"
+                                    }`}
+                            >
+                                {isSaving ? (
+                                    <AiOutlineLoading3Quarters className="animate-spin text-2xl" />
+                                ) : (
+                                    "Save"
+                                )}
+                            </button>
+                            <button
+                                onClick={() => copyGenerate(currentPpl)}
+                                className="w-full text-white font-bold py-3 px-4 rounded-lg cursor-pointer bg-blue-600 hover:bg-blue-700 active:scale-95 transition-all duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                            >
+                                Generate Message
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Home;
--- app/ts/server.ts
+++ app/ts/server.ts
@@ -3,87 +3,123 @@ import { PersonData } from "../types";
 
 // Get all people
 export const getPeople = async () => {
-	const { data: currentPeople, error: errorCurr } = await supabase.from("current_people").select("*");
-	const { data: defaultPeople, error: errorDef } = await supabase.from("default_people").select("*");
+    const { data: currentPeople, error: errorCurr } = await supabase
+        .from("current_people")
+        .select("*");
+    const { data: defaultPeople, error: errorDef } = await supabase
+        .from("default_people")
+        .select("*");
 
-	if (errorCurr) throw errorCurr;
-	if (errorDef) throw errorDef;
+    if (errorCurr) throw errorCurr;
+    if (errorDef) throw errorDef;
 
-	return {
-		currentPeople: currentPeople || [],
-		defaultPeople: defaultPeople || [],
-	};
+    return {
+        currentPeople: currentPeople || [],
+        defaultPeople: defaultPeople || [],
+    };
 };
 
 // Insert 1 into current people
 export const addOneCurrPpl = async (person: PersonData) => {
-	const { data, error } = await supabase.from("current_people").insert(person).select();
+    const { data, error } = await supabase
+        .from("current_people")
+        .insert(person)
+        .select();
 
-	error ? console.error("Error inserting: " + person.name, error) : console.log("Inserted:", person.name);
+    error
+        ? console.error("Error inserting: " + person.name, error)
+        : console.log("Inserted:", person.name);
 };
 
 // Update 1 from current people
 export const updateOneCurrPpl = async (person: PersonData) => {
-	const { data, error } = await supabase.from("current_people").update(person).eq("id", person.id).select();
+    const { data, error } = await supabase
+        .from("current_people")
+        .update(person)
+        .eq("id", person.id)
+        .select();
 
-	error ? console.error("Error updating: " + person.name, error) : console.log("Updated:", person.name);
+    error
+        ? console.error("Error updating: " + person.name, error)
+        : console.log("Updated:", person.name);
 };
 
 // Delete 1 from current people
 export const deleteOneCurrPpl = async (id: string) => {
-	const { data, error } = await supabase.from("current_people").delete().eq("id", id);
+    const { data, error } = await supabase
+        .from("current_people")
+        .delete()
+        .eq("id", id);
 
-	error ? console.error("Error deleting: " + data, error) : console.log("Deleted:", data);
+    error
+        ? console.error("Error deleting: " + data, error)
+        : console.log("Deleted:", data);
 };
 
 // Reset current people
 export const postCurrPpl = async (people: PersonData[]) => {
-	// Clear table first
-	await supabase.from("current_people").delete().neq("id", 0);
+    // Clear table first
+    await supabase.from("current_people").delete().neq("id", 0);
 
-	const { data, error } = await supabase.from("current_people").insert(people).select();
+    const { data, error } = await supabase
+        .from("current_people")
+        .insert(people)
+        .select();
 
-	if (error) {
-		console.error("Error saving current people", people, error);
-		alert("Failed to save");
-		return false;
-	} else {
-		console.log("Successfully reset current people");
-		return true;
-	}
+    if (error) {
+        console.error("Error saving current people", people, error);
+        alert("Failed to save");
+        return false;
+    } else {
+        console.log("Successfully reset current people");
+        return true;
+    }
 };
 
 // Save default people
 export const postDefPpl = async (people: PersonData[]) => {
-	// Clear table first
-	await supabase.from("default_people").delete().neq("id", 0);
+    // Clear table first
+    await supabase.from("default_people").delete().neq("id", 0);
 
-	const { data, error } = await supabase.from("default_people").insert(people).select();
+    const { data, error } = await supabase
+        .from("default_people")
+        .insert(people)
+        .select();
 
-	if (error) {
-		console.error("Error saving default people", people, error);
-		alert("Failed to save");
-		return false;
-	} else {
-		console.log("Successfully reset default people");
-		return true;
-	}
+    if (error) {
+        console.error("Error saving default people", people, error);
+        alert("Failed to save");
+        return false;
+    } else {
+        console.log("Successfully reset default people");
+        return true;
+    }
 };
 
-export const currentPplSBListener = async () => {
-	const channels = supabase
-		.channel("custom-insert-channel")
-		.on("postgres_changes", { event: "INSERT", schema: "public", table: "current_people" }, payload => {
-			console.log("Current people table updated", payload);
-		})
-		.subscribe();
+export const currentPplChangeListener = async () => {
+    const channels = supabase
+        .channel("currentPpl-channel")
+        .on(
+            "postgres_changes",
+            { event: "*", schema: "public", table: "current_people" },
+            (payload) => {
+                alert("Current people has been updated. Please reload page ✨");
+                console.log("Current people table updated", payload);
+            }
+        )
+        .subscribe();
 };
 
-export const defaultPplSBListener = async () => {
-	const channels = supabase
-		.channel("custom-insert-channel")
-		.on("postgres_changes", { event: "INSERT", schema: "public", table: "default_people" }, payload => {
-			console.log("Default people table updated", payload);
-		})
-		.subscribe();
+export const defaultPplChangeListener = async () => {
+    const channels = supabase
+        .channel("defaultPpl-channel")
+        .on(
+            "postgres_changes",
+            { event: "*", schema: "public", table: "default_people" },
+            (payload) => {
+                alert("Default people has been updated. Please reload page ✨");
+                console.log("Default people table updated", payload);
+            }
+        )
+        .subscribe();
 };
